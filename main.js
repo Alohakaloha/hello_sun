bf411374cbf8ab023a4073666adc8d2ec7b16f6a
Trung <dnguyen@student.42wolfsburg.de>
2024-06-13 10:11:24 +0200

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+// let sun = document.getElementById('sun');
+
+// sun.addEventListener('click', widenSunshine());
+
+// function widenSunshine() {
+//     let boxShadowRegex = /^(.*?)\s(.*?)\s(.*?)\s(.*?)\s(rgb\(.*?\))$/;
+//     let matches = boxShadow.match(boxShadowRegex);
+// }
